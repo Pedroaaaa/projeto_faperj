fc99af5cf6f01116f19527b6eb48ba425dfc933a
Pedro <82984609+Pedroaaaa@users.noreply.github.com>
2022-03-08 09:18:01 -0300

    Update questionsRoutes.js

--- routes/questionsRoutes.js
+++ routes/questionsRoutes.js
@@ -1,8 +1,12 @@
 const QuestionController = require('../controllers/QuestionController');
 const ThemeController = require('../controllers/ThemeController');
-const CharacterController = require('../controllers/CharacterController')
+const CharacterController = require('../controllers/CharacterController');
+const ListController = require('../controllers/ListController');
 
 module.exports = app => {
+    app.route('/')
+        .get(ListController.readAll)
+
     app.route('/questions')
         .get(QuestionController.readAll)
         .post(QuestionController.create)
